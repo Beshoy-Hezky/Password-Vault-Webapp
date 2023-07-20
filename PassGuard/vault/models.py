4973dc89e0d59e6eddcbaea5add28d08da9deec8
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-07-20 18:03:39 -0400

    F03: Add model for password in databse

--- PassGuard/vault/models.py
+++ PassGuard/vault/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class password(models.Model):
+    title = models.CharField(max_length=32)
+    website = models.CharField(max_length=32)
+    notes = models.CharField(max_length=150)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="according_passwords")
+
+    def __str__(self):
+        return f"{self.title} password by {self.owner}"
