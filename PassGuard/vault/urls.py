87bfd87f6c071e789c887c56617a866815da1086
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-07-22 11:19:15 -0400

    F05, F06: Add paths to reveal password and delete password

--- PassGuard/vault/urls.py
+++ PassGuard/vault/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
     path("aboutus/", views.about_us, name="about_us"),
-    path("addpassword/", views.add_password, name="add_password")
+    path("addpassword/", views.add_password, name="add_password"),
+    path("deletepassword/", views.delete_password, name="delete_password"),
+    path("reveal/<int:id>", views.reveal, name="reveal")
 ]
